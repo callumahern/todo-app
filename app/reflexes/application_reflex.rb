e6420fe2c012f05590a2411b402cd52c17cf6b4d
callumahern <callum.ahern@simplybusiness.co.uk>
2023-03-23 12:33:26 +0000

    webpacker install

--- app/reflexes/application_reflex.rb
+++ app/reflexes/application_reflex.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationReflex < StimulusReflex::Reflex
-  def mark_complete
-    todo_item = TodoItem.find(element.dataset.id)
-    todo_item.update(completed: true, completed_at: Time.now)
-  end
-
-  def mark_incomplete
-    todo_item = TodoItem.find(element.dataset.id)
-    todo_item.update(completed: false, completed_at: nil)
-  end
+  # Put application-wide Reflex behavior and callbacks in this file. 
+  #
+  # Example:
+  #
+  #   # If your ActionCable connection is: `identified_by :current_user`
+  #   delegate :current_user, to: :connection
+  #
+  # Learn more at: https://docs.stimulusreflex.com/reflexes#reflex-classes
 end
